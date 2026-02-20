c7b4841a6e38ae6d988461f9b7201ddeb806dfae
priyanshumaurya700 <priyanshupratapm@gmail.com>
2026-02-20 15:38:04 +0530

    fixing the error

--- src/app/viewProfile/page.tsx
+++ src/app/viewProfile/page.tsx
@@ -3,17 +3,24 @@
 import StudentProfile from "@/components/dashboard/student/StudentProfile";
 import TeacherProfile from "@/components/dashboard/teacher/TeacherProfile";
 import { useEffect, useState } from "react";
+interface User {
+  name: string;
+  email: string;
+  role: "student" | "teacher" | "admin";
+}
 
 export default function ViewProfilePage() {
-  const [user, setUser] = useState<any>(null);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     // Example: localStorage se fetch
-    const storedUser = JSON.parse(localStorage.getItem("role") || "{}");
-    setUser(storedUser);
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
   }, []);
 
-  if (!user) return <div>Loading...</div>;
+  if (!user || !user.role) return <div>Loading...</div>;
 
   return (
     <div className="p-6">
